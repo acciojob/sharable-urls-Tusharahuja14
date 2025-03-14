a7e2513288e1ceb19bed24dbbb4911e82e370744
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-03-14 23:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+let name=document.getElementById("name");
+let year=document.getElementById("year");
+let display=document.getElementById("display");
+let btn=document.getElementById("button");
+function show(){
+	let nameValue = name.value.trim(); // Get input values and trim extra spaces
+    let yearValue = year.value.trim();
+if(nameValue===""&&yearValue.length>0)
+{
+	display.innerText=`https://localhost:8080/?year=${yearValue}`;
+}
+else if(nameValue.length>0&&yearValue==="")
+{
+		display.innerText=`https://localhost:8080/?name=${nameValue}`;
+}
+else if(nameValue.length>0&&yearValue.length>0)
+{
+			display.innerText=`https://localhost:8080/?name=${nameValue}&year=${yearValue}`;
+}
+}
+
+btn.addEventListener("click",show);
